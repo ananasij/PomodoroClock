528abdcdd8a194a0d985aedead2ddfc945cb7f18
Hanna Senkevich <hanna.senkevich@gmail.com>
2017-01-06 12:44:26 +0100

    Small updates for more stable work

--- main.js
+++ main.js
@@ -2,12 +2,12 @@ var pomodoro;
 
 function init() {
     pomodoro = new Pomodoro();
-    pomodoro.onTick = renderTimer;
-    pomodoro.onSwitch = renderTimerName;
+    pomodoro.on('tick', renderTimer);
+    pomodoro.on('switch', renderCurrentTimerName);
 
     renderTimerLength('workTimer');
     renderTimerLength('restTimer');
-    renderTimerName('Work session');
+    renderCurrentTimerName();
     renderTimer();
 
     $('.js-btn-updateTimer').on('click', function(e) {
@@ -60,7 +60,14 @@ function formatTime(seconds) {
     return prettyTime;
 }
 
-function renderTimerName(currentTimer) {
+function renderCurrentTimerName() {
+    var currentTimer = '';
+    if (pomodoro.phase === Pomodoro.PHASE_WORK) {
+        currentTimer = 'Work session';
+    } else {
+        currentTimer = 'Rest';
+    }
+
     $('.js-currentTimer').text(currentTimer);
 }
 
@@ -68,65 +75,76 @@ $(document).ready(init);
 
 // Pomodoro
 
-var STATE_COUNTDOWN = 'STATE_COUNTDOWN';
-var STATE_PAUSE = 'STATE_PAUSE';
-var PHASE_WORK = 'PHASE_WORK';
-var PHASE_REST = 'PHASE_REST';
-var TIMER_DELAY = 1000;
-var TIMER_UPDATE_SCALE = 60;
-
-
 function Pomodoro() {
-    this.state = STATE_PAUSE;
-    this.phase = PHASE_WORK;
-    this.workTimer = 0.2 * 60;
+    this.state = Pomodoro.STATE_PAUSE;
+    this.phase = Pomodoro.PHASE_WORK;
+    this.workTimer = 0.15 * 60;
     this.restTimer = 0.1 * 60;
+    this.callbacks = {
+        tick: null,
+        switch: null
+    };
     this.resetCountdown();
 }
+
+Pomodoro.STATE_COUNTDOWN = 'STATE_COUNTDOWN';
+Pomodoro.STATE_PAUSE = 'STATE_PAUSE';
+Pomodoro.PHASE_WORK = 'PHASE_WORK';
+Pomodoro.PHASE_REST = 'PHASE_REST';
+Pomodoro.TIMER_DELAY = 1000;
+Pomodoro.TIMER_UPDATE_SCALE = 60;
 
 Pomodoro.prototype.resetCountdown = function() {
     this.timeLeft = this.workTimer;
 };
 
 Pomodoro.prototype.updateTimer = function(timer, update) {
-    if (update > 0 || this[timer] > TIMER_UPDATE_SCALE) {
-        this[timer] += update * TIMER_UPDATE_SCALE;
+    if (update > 0 || this[timer] > Pomodoro.TIMER_UPDATE_SCALE) {
+        this[timer] += update * Pomodoro.TIMER_UPDATE_SCALE;
     }
 };
 
 Pomodoro.prototype.switchTimer = function() {
-    if (this.state === STATE_PAUSE) {
+    if (this.state === Pomodoro.STATE_PAUSE) {
         this.startTimer();
-        this.state = STATE_COUNTDOWN;
+        this.state = Pomodoro.STATE_COUNTDOWN;
     } else {
         clearInterval(this.timerID);
-        this.state = STATE_PAUSE;
+        this.state = Pomodoro.STATE_PAUSE;
     }
 };
 
 Pomodoro.prototype.startTimer = function() {
-    this.state = STATE_COUNTDOWN;
-    this.timerID = setInterval(this.tick.bind(this), TIMER_DELAY);
+    this.state = Pomodoro.STATE_COUNTDOWN;
+    this.timerID = setInterval(this.tick.bind(this), Pomodoro.TIMER_DELAY);
 };
 
 Pomodoro.prototype.tick = function() {
-    this.onTick();
-    this.timeLeft -= TIMER_DELAY / 1000;
     if (this.timeLeft < 0) {
-        clearInterval(this.timerID);
         this.switchPhase();
-        this.startTimer();
     }
+    this.trigger('tick');
+    this.timeLeft -= Pomodoro.TIMER_DELAY / 1000;
 };
 
 Pomodoro.prototype.switchPhase = function() {
-    if (this.phase === PHASE_WORK) {
-        this.phase = PHASE_REST;
+    if (this.phase === Pomodoro.PHASE_WORK) {
+        this.phase = Pomodoro.PHASE_REST;
         this.timeLeft = this.restTimer;
-        this.onSwitch('Rest');
+        this.trigger('switch');
     } else {
-        this.phase = PHASE_WORK;
+        this.phase = Pomodoro.PHASE_WORK;
         this.timeLeft = this.workTimer;
-        this.onSwitch('Work session');
+        this.trigger('switch');
+    }
+};
+
+Pomodoro.prototype.on = function(eventName, callback) {
+    this.callbacks[eventName] = callback;
+};
+
+Pomodoro.prototype.trigger = function(eventName, value) {
+    if (this.callbacks[eventName]) {
+        this.callbacks[eventName](value);
     }
 };
